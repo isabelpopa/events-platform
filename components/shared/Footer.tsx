52563f5712205c5976f90bbcb58b96c1c033ea7a
isabelpopa <isabel.popa@outlook.com>
2024-05-03 16:52:48 +0100

    first commit

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <p>Events Platform 2024. All Rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
